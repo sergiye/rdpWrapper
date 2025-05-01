e2ae3700452d6ce26dfa54508c380d3fafb782bd
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2025-05-01 19:12:06 +0300

    fx

---
+++ rdpWrapper/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RDP Wrapper")]
+[assembly: AssemblyDescription("RDP Wrapper setup and configuration utility")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sergiy Egoshyn")]
+[assembly: AssemblyProduct("RDPWrapper")]
+[assembly: AssemblyCopyright("Copyright © Sergiy Egoshyn 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3768507d-4e38-4ec7-8f1d-c9347ac45a5e")]
+
+[assembly: AssemblyVersion("1.0.*")]
